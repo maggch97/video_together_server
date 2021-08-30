10992c6c325f597efa792faf547269e8c038a067
Weijie Zhao <maggch@outlook.com>
2021-08-30 17:02:24 +0800

    1.0

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import enum
 import time
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -16,33 +17,74 @@ class Play(BaseModel):
     lastUpdateTime: Optional[int]
     originalCurrent: Optional[int]
     playbackRate: Optional[float] = 1
+    paused: Optional[bool]
 
 
 class Viedo(BaseModel):
     link: Optional[str]
 
 
-class Room(BaseModel):
+class Role(enum.Enum):
+    MASTER = 1
+    GREEN = 2
+
+
+class RoomMeta(BaseModel):
     id: Optional[str]
     password: Optional[str]
+
+
+class Room(BaseModel):
+    meta: RoomMeta
     play: Optional[Play] = Play()
     video: Optional[Viedo] = Viedo()
     serverTime: Optional[int]
 
+    def PermissionCheck(self, role: Role, password: str):
+        if role == Role.MASTER:
+            if password != self.meta.password:
+                return False
+        print(password, self.meta.password)
+        return True
 
-database = {}
+
+class Response(BaseModel):
+    errCode: int
+    errMsg: str
+    data: Optional[Any]
+
+    @staticmethod
+    def Error(errCode: int, errMsg: str):
+        resp = Response(errCode=errCode, errMsg=errMsg)
+        return resp
+
+    @ staticmethod
+    def Ok(data: Any):
+        resp = Response(errCode=0, errMsg="ok")
+        resp.data = data
+        return resp
 
 
-@app.put('/room/{room_id}')
+database = dict()
+
+
+@ app.put('/room/{room_id}')
 def roomUpdate(room_id: str, room: Room):
+    if room_id in database:
+        if database[room_id].PermissionCheck(Role.MASTER, room.meta.password) is not True:
+            return Response.Error(1, "房间已存在，密码错误")
+    # print(room_id, room, database[room_id])
     database[room_id] = room
-    return room
+    # print(room_id, room, database[room_id])
+    return Response.Ok(room)
 
 
-@app.get('/room/{room_id}')
+@ app.get('/room/{room_id}')
 def roomGet(room_id: str):
+    if room_id not in database:
+        return Response.Error(2, "房间不存在")
     database[room_id].serverTime = time.time()
-    return database[room_id]
+    return Response.Ok(database[room_id])
 
 
 if __name__ == '__main__':
